88d2201bd571f56262be671ee0c1afb2433d9f7b
Aena Patel <aenapriyeshpatel25@gmail.com>
2026-02-21 09:42:09 +0530

    frontend setup completed with Tailwind

---
+++ client/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-blue-500 text-white flex items-center justify-center text-3xl">
+      Tailwind Working 🚀
+    </div>
+  );
+}
+
+export default App;
